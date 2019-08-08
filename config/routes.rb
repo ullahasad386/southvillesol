c80570a1c034940c149592b891f4bfabe465d4da
zamananjum20 <usama.ashraf.khan@gmail.com>
2019-08-08 20:04:24 +0500

    Added chatroom and chats model and controller, built homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  resources :chatrooms
+  resources :chats
+  get '/dashboard', to: 'chats#index'
+  root 'chatrooms#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
