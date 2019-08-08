21aa0eac4cac6ce1577aaf70f4e26084cd7eb3cd
zamananjum20 <usama.ashraf.khan@gmail.com>
2019-08-08 20:32:51 +0500

    Completed chat widget for southville sol

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,13 +1,32 @@
 class ChatsController < ApplicationController
   def index
+    @chatrooms = Chatroom.all
   end
-
-  def new
-  end
-
   def create
+    @chat = Chat.new(chat_params)
+    respond_to do |format|
+      if @chat.save
+        format.json { render :show, status: :created, location: @chat }
+      else
+        format.json { render json: @chat.errors, status: :unprocessable_entity }
+      end
+    end
   end
-
+  def new
+    @chat = Chat.new
+  end
   def show
+    @chats = Chat.where(chatroom_id: params[:id])
+    respond_to do |format|
+      if @chats
+        format.json { render :chats, status: :ok }
+      else
+        format.json { render json: @chats.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+  private
+  def chat_params
+    params.require(:chat).permit(:message, :name, :chatroom_id)
   end
 end
--- app/models/chatroom.rb
+++ app/models/chatroom.rb
@@ -1,2 +1,8 @@
 class Chatroom < ApplicationRecord
+  after_create :notify_pusher
+  has_many :chats
+
+  def notify_pusher
+    Pusher.trigger('chat', 'new-chatroom', self.as_json)
+  end
 end
